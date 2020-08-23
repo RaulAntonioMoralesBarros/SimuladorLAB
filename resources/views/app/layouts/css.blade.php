aa011729f7be6cf2abec401a58e9fa73445df55a
Raúl Antonio <raulantoniomb1994@gmail.com>
2020-08-22 22:38:36 -0400

    Se estructuro las partes de una pág web del proyecto

---
+++ resources/views/app/layouts/css.blade.php
@@ -0,0 +1,14 @@
+   <!--=================================================>
+ <===================| THEME STYLES |=================>
+ <==================================================-->
+
+
+   <link href="{{ URL::asset('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700') }}"
+       rel="stylesheet" />
+
+   <link href="{{ URL::asset('../css/pages/wizard/wizard-1.css?v=7.0.6') }}" rel="stylesheet" />
+   <link href="{{ URL::asset('../plugins/global/plugins.bundle.css?v=7.0.6') }}" rel="stylesheet" />
+   <link href="{{ URL::asset('../plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6') }}" rel="stylesheet" />
+   <link href="{{ URL::asset('../css/style.bundle.css?v=7.0.6') }}" rel="stylesheet" />
+
+   <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
---
+++ resources/views/app/layouts/js.blade.php
@@ -0,0 +1,6 @@
+ {{-- JS --}}
+ <script src="{{ URL::asset('../plugins/global/plugins.bundle.js?v=7.0.6') }}"></script>
+ <script src="{{ URL::asset('../plugins/custom/prismjs/prismjs.bundle.js?v=7.0.6') }}"></script>
+ <script src="{{ URL::asset('../js/scripts.bundle.js?v=7.0.6') }}"></script>
+ 
+ {{-- /JS --}}
