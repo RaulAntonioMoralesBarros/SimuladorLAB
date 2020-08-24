6bd87e57e23ac4a72b20910993c0b273e0e4e7fa
Nicol Garrido <ngarridogarrido7@gmail.com>
2020-08-24 16:50:13 -0400

    se agrego los js y css

---
+++ resources/views/plantilla/css.blade.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="{{ URL::asset('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700')}}"/>
+<link href="{{URL::asset('../css/pages/wizard/wizard-4.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
+<link href="{{URL::asset('../plugins/global/plugins.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link href="{{URL::asset('../plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link href="{{URL::asset('../css/style.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link rel="shortcut icon" href="{{URL::asset('../media/logos/favicon.ico')}}"/>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Agregar_usuario', function () {
+    return view('plantilla.app');
+});
